48526263cb253e514158c8edec7177bc0d5b7458
Santiago <Martnez Bota>
2021-10-31 17:06:31 +0100

    comentarios aclarativos del codigo

--- cap2/mostrarValoresClase/Retorno.php
+++ cap2/mostrarValoresClase/Retorno.php
@@ -1,16 +1,40 @@
 <?php
 
+/**
+ * @Retorno esta clase tiene tres atributos privados
+ * que devolveran los valroes mediante los metodos magicos
+ * __set() y __get()
+ */
 class Retorno
 {
-private $a;
-private $b;
-private $c;
+    /**
+     * @var mixed las variables no contienen control especifico
+     * de tipos
+     */
+    private $a;
+    private $b;
+    private $c;
 
-public function __set($name, $value){
-    $this->b = $value[0];
-    $this->c = $value[1];
-}
-public function __get($property){
-    return $this->b. ", ".$this->c;
-}
+    /**
+     * @param $name mixed por defecto del set
+     * @param $value mixed calor que le pasemos
+     */
+
+    public function __set($name, $value)
+    {
+        /**
+         * asignacion de los valores a b y c desde un array
+         */
+        $this->b = $value[0];
+        $this->c = $value[1];
+    }
+
+    /**
+     * @param $property mixed por defecto del get
+     * @return string devuelve en string los valores asignados previamente
+     */
+    public function __get($property)
+    {
+        return $this->b . ", " . $this->c;
+    }
 }
--- cap2/mostrarValoresClase/index.php
+++ cap2/mostrarValoresClase/index.php
@@ -6,16 +6,38 @@
 </head>
 <body>
 <?php
-
-function cargarClase($nombre_clase){
-    include $nombre_clase.".php";
+/**
+ * @param $nombre_clase string parametro que recibe el nombre de la clase
+ */
+function cargarClase($nombre_clase)
+{
+    /**
+     * buscamos en el mismo directorio fichero con nombre del param
+     */
+    include $nombre_clase . ".php";
 }
 
+/**
+ * registro del autoload (PHP 8.0)
+ */
 spl_autoload_register("cargarClase");
+
+/**
+ * instancia de la clase
+ */
 
 $retorno = new Retorno();
 
+/**
+ * disparador del __set()
+ */
+
 $retorno->setter = [6, 9];
+
+/**
+ * disparador del get
+ */
+
 echo $retorno->setter;
 
 ?>
